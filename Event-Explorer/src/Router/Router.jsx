44fe0791f0552158f7e60f1d38b08210d8eb4e67
noob-ubaid <ubaidurrahman661@gmail.com>
2025-05-04 19:41:46 +0200

    Project setup done

---
+++ Event-Explorer/src/Router/Router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter} from "react-router";
+import Root from "../pages/Root/Root";
+import Home from "../pages/Home/Home";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root ,
+    children : [
+        {
+            index : true,
+            Component : Home
+        }
+    ]
+  },
+]);
---
+++ Event-Explorer/src/pages/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const Root = () => {
+    return (
+        <div>
+           root 
+           <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
